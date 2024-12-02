3d38f8c3e7c6448add88047c51bce053cb2df20d
24hs211 <24hs211@s.bgu.ac.jp>
2024-12-02 13:57:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
         function checkFortune() {
             var colour = document.getElementById('colour-select').value;
             var number = parseInt(document.getElementById('number-input').value, 10);
-            var result = "凶"; // デフォルトの値
+            var result ="コイン"; // デフォルトの値
 
-            if (colour === "黒" && number === 0) {
-                result = "小吉";
-            } else if (colour === "黒" && number === 1) {
-                result = "中吉";
-            } else if (colour === "黒" && number === 2) {
-                result = "凶";
-            } else if (colour === "白" && number === 0) {
-                result = "中吉";
-            } else if (colour === "白" && number === 1) {
-                result = "中吉";
-            } else if (colour === "白" && number === 2) {
-                result = "大凶";
-            } else if (colour === "オレンジ" && number === 0) {
-                result = "凶";
-            } else if (colour === "オレンジ" && number === 1) {
-                result = "大吉";
-            } else if (colour === "オレンジ" && number === 2) {
-                result = "中吉";
+            if (colour === "赤" && number === 0) {
+                result = "キラー";
+            } else if (colour === "赤" && number === 1) {
+                result = "スター";
+            } else if (colour === "赤" && number === 2) {
+                result = "キノコ";
+            } else if (colour === "青" && number === 0) {
+                result = "みどり甲羅";
+            } else if (colour === "青" && number === 1) {
+                result = "バナナ";
+            } else if (colour === "青" && number === 2) {
+                result = "サンダー";
+            } else if (colour === "黄色" && number === 0) {
+                result = "トゲ甲羅";
+            } else if (colour === "黄色" && number === 1) {
+                result = "赤甲羅";
+            } else if (colour === "黄色" && number === 2) {
+                result = "ゲッソー";
             }
 
-            document.getElementById('result-output').innerText = "あなたの今週の運勢は【" + result + "】です";
+            document.getElementById('result-output').innerText = "あなたの今週のアイテムは【" + result + "】です";
         }
